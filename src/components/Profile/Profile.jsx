7d48eab3f276851b296f428e1270fe65eca03ed1
Adamyuyue <67887367+Adamyuyue@users.noreply.github.com>
2023-03-10 23:30:15 -0600

    added favorite and watchlist

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,20 +3,37 @@ import { Typography, Button, Box } from "@mui/material";
 import { useSelector } from "react-redux";
 import { ExitToApp } from "@mui/icons-material";
 import { userSelector } from "../../features/auth";
+import { useGetListQuery } from "../../services/TMDB";
+import {RatedCards} from '..'
 // Get access to profile name or id from redux state
 // display in the profile component
 
 const Profile = () => {
   const { user } = useSelector(userSelector);
 
-  const favoriteMovies = []
+  const { data: favoriteMovies, refetch: refectchFavorites } = useGetListQuery({
+    listName: "favorite/movies",
+    accountId: user.id,
+    sessionId: localStorage.getItem("session_id"),
+    page: 1,
+  });
+
+  const { data: watchlistMovies, refetch: refetchWatchlisted } = useGetListQuery({
+    listName: "watchlist/movies",
+    accountId: user.id,
+    sessionId: localStorage.getItem("session_id"),
+    page: 1,
+  });
+
+  useEffect(()=> {
+    refectchFavorites();
+    refetchWatchlisted();
+  })
 
   const logout = () => {
     localStorage.clear();
-    window.location.href = '/';
+    window.location.href = "/";
   };
-
-
 
   return (
     <Box>
@@ -28,12 +45,16 @@ const Profile = () => {
           Logout &nbsp; <ExitToApp />
         </Button>
       </Box>
-      {!favoriteMovies.length 
-      ? <Typography variant="h5">Add favorites or watchlist some movies to see them here!</Typography>
-      : <Box>
-        FAVORITE MOVIES
-      </Box>
-      }
+      {!favoriteMovies?.results?.length && !watchlistMovies?.results?.length ? (
+        <Typography variant="h5">
+          Add favorites or watchlist some movies to see them here!
+        </Typography>
+      ) : (
+        <Box>
+          <RatedCards title="Favorite Movies" data={favoriteMovies} />
+          <RatedCards title="Watchlist" data={watchlistMovies} />
+        </Box>
+      )}
     </Box>
   );
 };
